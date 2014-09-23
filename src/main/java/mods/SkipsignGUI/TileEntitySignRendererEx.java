fb90410b26a17b362c86c3416514331a439e2cec
TTTA2 <nanasisine@gmail.com>
2014-09-24 03:37:39 +0900

    アップデート 201409240337

--- src/main/java/mods/SkipsignGUI/TileEntitySignRendererEx.java
+++ src/main/java/mods/SkipsignGUI/TileEntitySignRendererEx.java
@@ -1,3 +1,14 @@
+﻿/*
+ * SkipSign
+ * 
+ * (c) 2014 TTTA2
+ * https://github.com/TTTA2/SkipSignGUI
+ * 
+ * This mod is distributed under the terms of the Minecraft Mod Public License 1.0, or MMPL.
+ * Please check the contents of the license located in http://www.mod-buildcraft.com/MMPL-1.0.txt
+ * この MOD は、Minecraft Mod Public License (MMPL) 1.0 の条件のもとに配布されています。
+ * ライセンスの内容は次のサイトを確認してください。 http://www.mod-buildcraft.com/MMPL-1.0.txt
+ */
 package mods.SkipsignGUI;
 
 import java.util.Iterator;
